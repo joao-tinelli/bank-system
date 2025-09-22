94fb99a4e9c5d685c0b19f8254e26cb40e76be14
Joao Tinelli <joao_tinelli@outlook.com>
2025-09-22 10:40:28 -0300

    Documentation using Open API

--- cards/src/main/java/com/joaotinelli/cards/CardsApplication.java
+++ cards/src/main/java/com/joaotinelli/cards/CardsApplication.java
@@ -1,11 +1,29 @@
 package com.joaotinelli.cards;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "JoaoBank Cards microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Joao Tinelli",
+                        email = "joaotinelli@alunos.utfpr.edu.br"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        )
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
