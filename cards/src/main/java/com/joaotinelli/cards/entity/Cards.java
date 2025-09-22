bd75b55da72f74b1bb9ad0cbe75cd082cc078cbe
Joao Tinelli <joao_tinelli@outlook.com>
2025-09-22 09:51:05 -0300

    [Cards] Add cards  microservice with entities, configuration and schema

---
+++ cards/src/main/java/com/joaotinelli/cards/entity/Cards.java
@@ -0,0 +1,30 @@
+package com.joaotinelli.cards.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cards extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cardId;
+
+    private String mobileNumber;
+
+    private String cardNumber;
+
+    private String cardType;
+
+    private int totalLimit;
+
+    private int amountUsed;
+
+    private int availableAmount;
+}
---
+++ cards/src/test/java/com/joaotinelli/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaotinelli.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
