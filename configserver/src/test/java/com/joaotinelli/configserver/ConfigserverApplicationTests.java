c03f53b5e372172806a59c459a160bde7e5260a9
Joao Tinelli <joao_tinelli@outlook.com>
2025-10-23 14:09:09 -0300

    [Spring Cloud] Microservices now fetch the environment's configurations from the Config Server

---
+++ configserver/src/test/java/com/joaotinelli/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaotinelli.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
